477b4d5967e7e2a71d70464d67769f138198415f
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-04 13:04:58 -0600

    Change on user input

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
   var superName = document.querySelector('.super-name');
   var colorChangers = Array.from(document.querySelectorAll('.change-color'));
   var resetBtn = document.querySelector('.reset-color');
+  var superFormName = document.querySelector('.super-form__name');
 
   var registerClick = (colorBtn) => {
     var changeSuperColor = () => {
@@ -18,4 +19,8 @@
   colorChangers.forEach(registerClick);
 
   registerClick(resetBtn);
+
+  superFormName.addEventListener('input', () => {
+    superName.innerHTML = `My Name is: ${superFormName.value}`;
+  });
 })();
