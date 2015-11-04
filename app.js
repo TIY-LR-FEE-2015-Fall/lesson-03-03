fb687333be114aed1590a8f6ee65846c1bb5196e
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-04 11:25:13 -0600

    Color Change Buttons

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 (function() {
+  var superName = document.querySelector('.super-name');
+  var colorChangers = Array.from(document.querySelectorAll('.change-color'));
 
+  var registerClick = (colorBtn) => {
+    var changeSuperColor = () => {
+      superName.style.color = colorBtn.innerHTML;
+    };
+
+    colorBtn.addEventListener('click', changeSuperColor);
+  };
+
+  colorChangers.forEach(registerClick);
 })();
