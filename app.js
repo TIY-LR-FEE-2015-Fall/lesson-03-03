fbf976fac0f60892f403a0bdd2e16200ddf0a338
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-04 11:40:09 -0600

    Allow Color Reset

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 (function() {
   var superName = document.querySelector('.super-name');
   var colorChangers = Array.from(document.querySelectorAll('.change-color'));
+  var resetBtn = document.querySelector('.reset-color');
 
   var registerClick = (colorBtn) => {
     var changeSuperColor = () => {
-      superName.style.color = colorBtn.innerHTML;
+      if (colorBtn.innerHTML === 'Reset') {
+        superName.style.color = '';
+      } else {
+        superName.style.color = colorBtn.innerHTML;
+      }
     };
 
     colorBtn.addEventListener('click', changeSuperColor);
   };
 
   colorChangers.forEach(registerClick);
+
+  registerClick(resetBtn);
 })();
